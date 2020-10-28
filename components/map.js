972d4b18f5ab79c4696c83b019ff111104b7e945
Yasuhiko Nara <narayasuhiko3@gmail.com>
2020-10-28 10:01:38 +0900

    successed in saving environment valkues

--- components/map.js
+++ components/map.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import { GOOGLE_MAPS_API_KEY } from '@env';
 
 export default function () {
-  const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+  const origin = { latitude: 35.681236, longitude: 139.767125 };
+  const destination = { latitude: 35.6, longitude: 139.7 };
 
   return (
     <MapView
@@ -19,12 +22,17 @@ export default function () {
     >
       <MapView.Marker
         coordinate={{
-          latitude: 35.681236,
-          longitude: 139.767125,
+          latitude: 35.68,
+          longitude: 139.76,
         }}
         title={'東京駅'}
         description={'JRの駅です。'}
         onPress={() => alert('click')}
+      />
+      <MapViewDirections
+        apikey={GOOGLE_MAPS_API_KEY}
+        origin={origin}
+        destination={destination}
       />
     </MapView>
   );
