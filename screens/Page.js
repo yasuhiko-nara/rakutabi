8a26514659ed2c86f80ef5941870825935d3396c
Yasuhiko Nara <narayasuhiko3@gmail.com>
2020-10-29 10:13:31 +0900

    added tab-navigator

--- screens/Page.js
+++ screens/Page.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './HomeScreen';
+
+const Tab = createBottomTabNavigator();
+
 export default function Page({ navigation }) {
   return (
-    <TouchableOpacity
-      onPress={() => navigation.navigate('Home')}
-      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-    >
-      <Text>Page</Text>
-    </TouchableOpacity>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Page" component={Page} />
+    </Tab.Navigator>
   );
 }
